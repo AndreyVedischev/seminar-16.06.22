54e16971640c974259b599421a4aff4bcb0ce28a
Andrey Vedischev <vedischev76@mail.ru>
2022-06-16 23:07:00 +0500

    добавили задачу1

---
+++ задача1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите число: ");
+int N = Convert.ToInt32(ReadLine());
+WriteLine(GetCount(N));
+int GetCount(int a)
+{
+    int result = 0;
+    while (a > 1)
+    {
+        a = a / 10;
+        result++;
+    }
+    return result;
+}
