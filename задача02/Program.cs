cd16b0d79e3c0753b9ca94919ea8df353cfe24c3
Andrey Vedischev <vedischev76@mail.ru>
2022-06-16 23:23:17 +0500

    добавили задачу02

---
+++ задача02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+
+Clear();
+Write("Введите число: ");
+WriteLine($"Произведение чисел равно {Getn(Convert.ToInt32(ReadLine()))}");
+
+int Getn(int n)
+{
+    int result = 1;                      //можно поставить Int 64 для больших чисел.
+    for(int i = 1; i<=n; i++)
+    {
+        result*= i;
+    }
+    return result;
+}
