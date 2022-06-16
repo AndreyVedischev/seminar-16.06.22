c11dc1d1e87c974ed5672e32adda4fc69e36398a
Andrey Vedischev <vedischev76@mail.ru>
2022-06-16 22:42:33 +0500

    добавили задачу22

---
+++ задача22/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+ 
+Clear();
+Write("Введите число: ");
+int N = Convert.ToInt32(ReadLine());
+
+int sum = GetSum(N);
+WriteLine($"Сумма чисел = {sum}");
+
+//WriteLine($"Сумма чисел = {GetSum(Convert.ToInt32(ReadLine())}");
+
+int GetSum (int r)
+{
+    int result = 0;
+    for(int i = 1; i <= r; i++)
+    {
+        result+=i;
+    }
+    return result;
+}
