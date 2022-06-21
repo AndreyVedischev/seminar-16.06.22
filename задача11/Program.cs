a940e7e16edac6d4cfa2c736fa23086039a43827
Andrey Vedischev <vedischev76@mail.ru>
2022-06-21 20:57:04 +0500

    добавили задачу 11

---
+++ задача11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите число: ");
+//int N = Convert.ToInt32(ReadLine());
+WriteLine($"Количество цыфр = {GetCount(Convert.ToInt32(ReadLine()))}");
+
+int GetCount(int a)
+{
+    int result = 0;
+    for(int i = 0;i < a; i++)
+    {
+        a = a / 10;
+        result++;
+    }
+    return result;
+}
