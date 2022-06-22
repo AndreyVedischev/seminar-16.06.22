5ff04df589293feaf332a56c5cfabe7d725352e1
Andrey Vedischev <vedischev76@mail.ru>
2022-06-22 22:16:29 +0500

    добавили задачу 1111

---
+++ задача1111/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+WriteLine(GetLength(number));
+
+int GetLength(int num)
+{
+    int len = 0;
+    for(int i = 1; i <=num; i *= 10)
+    {
+        len++;
+    }
+    return len;
+}
