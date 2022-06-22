424242793d693816fdc2112f33e94c40e3a4e568
Andrey Vedischev <vedischev76@mail.ru>
2022-06-22 23:34:01 +0500

    добавили задачу 3

---
+++ задача3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int [] array = new int[8];
+FilArray(array);
+PrintArray(array);
+
+void FilArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1,8);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+}
