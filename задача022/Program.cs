cead09442224895e23e32f698dc2f0fdfddde236
Andrey Vedischev <vedischev76@mail.ru>
2022-06-22 22:38:47 +0500

    добавили задачу 022

---
+++ задача022/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+WriteLine($"Произведение чисел равно {Feban(Convert.ToInt32(ReadLine()))}");
+
+Int64 Feban(int n)
+{
+    int result = 1;
+    for(int i = 1; i <= n ; i++)
+    {
+        result *= i;
+    }
+    return result;
+
+}
